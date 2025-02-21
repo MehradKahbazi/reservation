bbb72d79a64d375cfca53b16ca46a3f074f05969
Mehrad <mehradhetfield@gmail.com>
2025-02-21 07:03:48 +0330

    initial commit

---
+++ app/create-reservation/page.js
@@ -0,0 +1,5 @@
+const CreateReservation = () =>{
+    return <h1>Create Reservation</h1>
+}
+
+export default CreateReservation;
---
+++ app/generate-report/page.js
@@ -0,0 +1,5 @@
+const GenerateReport = () =>{
+    return <h1>Generate Report</h1>
+}
+
+export default GenerateReport;
